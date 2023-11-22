ff063b8f785c251c8591281e2beb689f524a218c
ArjunLearningAcc <151787577+ArjunLearningAcc@users.noreply.github.com>
2023-11-23 02:37:20 +0530

    Update streamlit_app.py
    updated details on the headed and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry oat meal')
+streamlit.text('kale, spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
