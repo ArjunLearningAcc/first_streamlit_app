1594e4e18c63410ed472a7dc0fe1801502b5e775
ArjunLearningAcc <151787577+ArjunLearningAcc@users.noreply.github.com>
2023-11-27 23:37:38 +0530

    Update streamlit_app.py
    Updated code and added data import from api

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # working code properly -- testing done 
+
+streamlit.header("Fruityvice Fruit Advice!")
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
+
+
+
